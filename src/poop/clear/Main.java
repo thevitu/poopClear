f6a8dcbd0afa5e277d14511d6487a4ea94d39be5
Vitor Fernandes <vitor.fernandes@eproseed.com>
2019-08-19 10:32:41 +0100

    add findLogic

--- src/poop/clear/Main.java
+++ src/poop/clear/Main.java
@@ -5,14 +5,41 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Choose 1 to fix for-eachs 2 to find logic:");
+		try (var in = new Scanner(System.in)) {
+			var choosed = in.nextLine();
+			switch (choosed) {
+				case "1":
+					fixPoop();
+					break;
+				case "2":
+					findLogic();
+					break;
+			}
+		} catch (Throwable e) {
+			System.out.println(e);
+		}
+	}
+
+	private static void fixPoop() {
 		System.out.println("File:");
 		try (var in = new Scanner(System.in)) {
-			var clear = new PoopClear(in.nextLine());  
+			var clear = new PoopClear(in.nextLine());
 			clear.findPoop();
 			System.out.println("Fix?(Y or N)");
 			if ("Y".equalsIgnoreCase(in.nextLine())) {
 				clear.fixPoop();
 			}
+		} catch (Throwable e) {
+			System.out.println(e);
+		}
+	}
+
+	private static void findLogic() {
+		System.out.println("File:");
+		try (var in = new Scanner(System.in)) {
+			var clear = new PoopClear(in.nextLine());
+			clear.findLogic();
 		} catch (Throwable e) {
 			System.out.println(e);
 		}
